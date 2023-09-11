df886dc48dc1a10d6b31051ffcb138f634b73e26
Lam Phat <phat.vuhuynh@hcmut.edu.vn>
2023-09-11 15:46:20 +0700

    update real code

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -92,16 +92,19 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-  while (1){
-  	HAL_GPIO_WritePin ( YELLOW_LED_GPIO_Port , YELLOW_LED_Pin , GPIO_PIN_RESET ) ;
-  	HAL_GPIO_WritePin ( RED_LED_GPIO_Port , RED_LED_Pin ,
-  	GPIO_PIN_SET ) ;
-  	HAL_Delay (2000);
+bool state = 1;
+while (1){
+	if(state){
+		HAL_GPIO_WritePin ( YELLOW_LED_GPIO_Port , YELLOW_LED_Pin , GPIO_PIN_RESET ) ;
+		HAL_GPIO_WritePin ( RED_LED_GPIO_Port , RED_LED_Pin ,  	GPIO_PIN_SET ) ;
+	}
+	else{
+		HAL_GPIO_WritePin ( RED_LED_GPIO_Port , RED_LED_Pin ,  	GPIO_PIN_RESET ) ;
+		HAL_GPIO_WritePin ( YELLOW_LED_GPIO_Port , YELLOW_LED_Pin , GPIO_PIN_SET ) ;
+	}
 
-  	HAL_GPIO_WritePin ( RED_LED_GPIO_Port , RED_LED_Pin ,
-  	GPIO_PIN_RESET ) ;
-  	HAL_GPIO_WritePin ( YELLOW_LED_GPIO_Port , YELLOW_LED_Pin , GPIO_PIN_SET ) ;
-  	HAL_Delay (2000) ;
+	state = !state;
+	HAL_Delay (2000) ;
 
     /* USER CODE END WHILE */
 
@@ -158,16 +161,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, a0_Pin|a1_Pin|a2_Pin|a3_Pin
-                          |a4_Pin|a5_Pin|a6_Pin|a7_Pin
-                          |a8_Pin|a9_Pin|a10_Pin|a11_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin|YELLOW_LED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : a0_Pin a1_Pin a2_Pin a3_Pin
-                           a4_Pin a5_Pin a6_Pin a7_Pin
-                           a8_Pin a9_Pin a10_Pin a11_Pin */
-  GPIO_InitStruct.Pin = a0_Pin|a1_Pin|a2_Pin|a3_Pin
-                          |a4_Pin|a5_Pin|a6_Pin|a7_Pin
-                          |a8_Pin|a9_Pin|a10_Pin|a11_Pin;
+  /*Configure GPIO pins : RED_LED_Pin YELLOW_LED_Pin */
+  GPIO_InitStruct.Pin = RED_LED_Pin|YELLOW_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
